66902b6e60311feb2d1855ad86ed5c195d952083
superpozycja <anna@superpozycja.net>
2024-09-12 22:26:17 +0200

    test something

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,7 @@ fn main() {
     let pt = [0b0010_0110, 0b1011_0111];
     let key = [0b0011_1010, 0b1001_0100, 0b1101_0110, 0b0011_1111];
     let ct = la::encrypt(&pt,  &key);
+
+    println!("ciphertext:");
     la::print_block(&ct);
 }
